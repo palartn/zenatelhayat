12b8867390ce74849b04100b511e0b0f184b7f39
palart@gmail.com <palart@gmail.com>
2022-09-26 17:45:48 +0300

    migration

---
+++ database/migrations/2022_09_26_141437_create_patients_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->id();
+            $table->primary('idc'); //رقم هوية المريض
+            $table->integer('file_no')->unique(); //رقم الملف
+            $table->string('patient_fname'); // إسم المريض الأول
+            $table->string('patient_sname'); // إسم المريض الثاني
+            $table->string('patient_tname'); //  إسم المريض الثالث
+            $table->string('patient_lname'); // إسم المريض الرابع
+            $table->date('patient_dob'); // تاريخ الميلاد
+            $table->string('occupation'); // العمل
+            $table->string('address'); // العنوان
+            $table->integer('mobile'); // جوال
+            $table->integer('mobile_second'); // جوال ثاني
+            $table->boolean('gender'); // الجنس
+            $table->string('husband_name'); //اسم الزوج
+            $table->string('husband_occupation'); // عمل الزوج
+            $table->date('husband_dob'); // تاريخ ميلاد الزوج
+            $table->integer('husband_pic');  // صورة الزوج
+            $table->blob('notes'); // ملاحظات
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('patients');
+    }
+};
