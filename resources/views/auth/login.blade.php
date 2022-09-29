43b181ff806f9cbbf162ac1f4ddc6e12eb0c9771
Nael <palart@gmail.com>
2022-09-29 11:37:33 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,7 +2,7 @@
 <html lang="ar" dir="rtl">
 	<!--begin::Head-->
 	<head><base href="">
-		<title>Metronic - the world's #1 selling Bootstrap Admin Theme Ecosystem for HTML, Vue, React, Angular &amp; Laravel by Keenthemes</title>
+		<title>مركز زينة الحياة - تسجيل الدخول</title>
 		<meta name="description" content="The most advanced Bootstrap Admin Theme on Themeforest trusted by 94,000 beginners and professionals. Multi-demo, Dark Mode, RTL support and complete React, Angular, Vue &amp; Laravel versions. Grab your copy now and get life-time updates for free." />
 		<meta name="keywords" content="Metronic, bootstrap, bootstrap 5, Angular, VueJs, React, Laravel, admin themes, web design, figma, web development, free templates, free admin themes, bootstrap theme, bootstrap template, bootstrap dashboard, bootstrap dak mode, bootstrap button, bootstrap datepicker, bootstrap timepicker, fullcalendar, datatables, flaticon" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -48,7 +48,7 @@
 							<!--begin::Heading-->
 							<div class="text-center mb-10">
 								<!--begin::Title-->
-								<h1 class="text-dark mb-3 " style="font-family:Cairo;font-size:revert">مركز زينة الحياة</h1>
+								<h1 class="text-dark mb-3 " style="font-family:Cairo;font-size:revert ">مركز زينة الحياة</h1>
 								<!--end::Title-->
 								<!--begin::Link-->
 								{{-- <div class="text-gray-400 fw-bold fs-4">New Here?
