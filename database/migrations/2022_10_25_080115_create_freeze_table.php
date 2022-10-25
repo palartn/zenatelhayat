db06f164ad0eb01acbd0572db611923634f97496
Nael <palart@gmail.com>
2022-10-25 10:31:18 +0200

    freez

---
+++ database/migrations/2022_10_25_080115_create_freeze_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('freeze', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('file_id')->constrained('files')->cascadeOnDelete();
+            $table->integer('sample_count');
+            $table->enum('freez_type',['liquid','embryo'])->default('liquid');
+            $table->date('freez_begin_date');
+            $table->date('freez_end_date');
+            $table->double('total_price');
+            $table->double('currency');
+            $table->double('Payment');
+            $table->double('Remaining_amount');
+            $table->text('notes')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('freeze');
+    }
+};
