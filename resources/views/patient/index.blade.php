911f2ee4e34dcd05de1d913ade8679560e69b299
palart@gmail.com <palart@gmail.com>
2022-11-24 16:39:47 +0200

    theme

--- resources/views/patient/index.blade.php
+++ resources/views/patient/index.blade.php
@@ -54,12 +54,7 @@
                     </div>
                     <!--end::Toolbar-->
                     <!--begin::Group actions-->
-    {{--                <div class="d-flex justify-content-end align-items-center d-none" data-kt-docs-table-toolbar="selected">--}}
-    {{--                    <div class="fw-bolder me-5">--}}
-    {{--                        <span class="me-2" data-kt-docs-table-select="selected_count"></span>Selected</div>--}}
-    {{--                    <button type="button" class="btn btn-danger" data-kt-docs-table-select="delete_selected">Selection Action</button>--}}
-    {{--                </div>--}}
-                    <!--end::Group actions-->
+ 
                 </div>
                
             </div>
