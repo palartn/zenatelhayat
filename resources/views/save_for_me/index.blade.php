ba7ea5d0d637dcc24c48ae5d3c70864f3be48c49
Nael <palart@gmail.com>
2023-01-08 14:00:41 +0200

    00

--- resources/views/save_for_me/index.blade.php
+++ resources/views/save_for_me/index.blade.php
@@ -1,4 +1,4 @@
---سبب الزيارة
+{{-- --سبب الزيارة
 تجميد
 سائل
 
@@ -14,4 +14,34 @@
 اشعار انتهاء التجميد قبل  10 ايام
 
 --التقارير
+ --}}
+ {{-- $startdate = $request->startdate;
+$enddate = $request->enddate;
 
+$pdf = PDF::loadView('report.issuedreportprint',compact('stockIssued', 'startdate', 'enddate'));
+
+
+<p> From:{{ $startdate }}  To: {{ $enddate }} </p>
+ --}}
+"// date between search?"
+$from = date('2018-01-01');
+$to = date('2018-05-02');
+
+Reservation::whereBetween('reservation_from', [$from, $to])->get();
+
+
+Reservation::all()->filter(function($item) {
+  if (Carbon::now()->between($item->from, $item->to)) {
+    return $item;
+  }
+});
+
+
+Reservation::whereBetween('reservation_from', [$from1, $to1])
+  ->orWhereBetween('reservation_to', [$from2, $to2])
+  ->whereNotBetween('reservation_to', [$from3, $to3])
+  ->get();
+
+
+
+ 
